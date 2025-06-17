e9943d6074790fb44944327bb70156e36de50e1f
kantanvgat <kantan.vgat@gmail.com>
2025-06-17 12:35:55 +0900

    6/17 コミット　playerのジャンプの挙動

--- Assets/Script/Player/PlayerMove.cs
+++ Assets/Script/Player/PlayerMove.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float _PlayerJumpPower;
+    [SerializeField] bool JumpFrag = false;
+
+    Rigidbody2D player = default;
+    
     void Start()
     {
-        
+        player = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Jump"))
+        {
+            Debug.Log(JumpFrag);
+            if (JumpFrag == true)
+            {
+                player.AddForce(Vector2.up * _PlayerJumpPower, ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("GroundTag"))
+        {
+            JumpFrag = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("GroundTag"))
+        {
+            JumpFrag = false;
+        }
     }
 }
